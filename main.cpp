b7f63b9b31a517296cb51c8efc12df313d0d4851
ShantiGharib <gharibsj2020@student.laccd.edu>
2025-05-09 15:19:47 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,9 +16,15 @@ enum class MenuOptions {
 
 class PriorityQueue {
 private:
+    static const int GROWTH_FACTOR{2};
     int* pArr{nullptr};
-    size_t arrSize{0};
+    size_t arrSize{0}, maxSize{0};
     void Heapify();
+    bool Swap(int parent, int mxChild);
+    inline int GetGreatestChildIdx(int idxL, int idxR);
+    inline bool IsSizeLimited() const;
+    bool AllocateMemory(int* &pContainer);
+    void DeallocateMemory(int* &pContainer);
   
 public:
     
@@ -68,17 +74,74 @@ int main() {
 }
 
 
-void PriorityQueue::Heapify() {
+void PriorityQueue::Heapify(int idx) {
+    
+    const int leftChild = idx * 2;
+    const int rightChild = leftChild + 1;
+    
+    if (rightChild < arrSize) {
+        const int mxChild = GetGreatestChildIdx(leftChild, rightChild);
+        if (Swap(idx, mxChild)) {
+            Heapify(mxChild);
+        }
+
+    }
+    else if (leftChild < arrSize && Swap(idx, leftChild)) {
+        Heapify(leftChild);
+    }
+}
+
+
+inline int PriorityQueue::GetGreatestChildIdx(int idxL, int idxR) {
+    
+    return (pArr[idxL] >= pArr[idxR]) ? idxL : idxR;
     
 }
 
 
+bool PriorityQueue::Swap(int parent, int mxChild) {
+    
+    const bool result = pArr[parent] < pArr[mxChild]; 
+    
+    if (result) {
+            
+        const int temp = pArr[mxChild];
+        pArr[mxChild] = pArr[parent];
+        pArr[parent] = temp;
+        
+    }
+    
+    return result;
+    
+}
 
+bool PriorityQueue::AllocateMemory() {
+    int* pTemp = nullptr;
+    
+    try {
+        const int NEW_SIZE = max(1, currSize * 2);
+        pTemp = new int[NEW_SIZE];
+        for (int i = 0; i < currSize; ++i) {
+            pTemp[i] = pArr[i];
+        }
+        DeallocateMemory(pArr);
+        pArr = pTemp;
+    }
+    catch (std::bad_alloc &err) {
+        std::cout << "Not enough Continguous Memory Space to add more elements\n";
+        DeallocateMemory(pTemp);
+    }
+}
+void PriorityQueue::DeallocateMemory(int* &pContainer) {
+    if (pContainer != nullptr) {
+        delete[] pContainer;
+        pContainer = nullptr;
+    }
+} 
 
-
-
-
-
+inline bool PriorityQueue::IsSizeLimited() const {
+    return currSize < maxSize;
+}
 
 
 
